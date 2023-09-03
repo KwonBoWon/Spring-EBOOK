790fd6dcc943b095e0311aca25f24bdbc5117ff4
KwonBoWon <bogod1207@naver.com>
2023-09-03 23:49:41 +0900

    Chapter6

--- src/main/java/me/bowon/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/bowon/springbootdeveloper/controller/BlogApiController.java
@@ -4,12 +4,14 @@ package me.bowon.springbootdeveloper.controller;
 import lombok.RequiredArgsConstructor;
 import me.bowon.springbootdeveloper.domain.Article;
 import me.bowon.springbootdeveloper.dto.AddArticleRequest;
+import me.bowon.springbootdeveloper.dto.ArticleResponse;
+import me.bowon.springbootdeveloper.dto.UpdateArticleRequest;
 import me.bowon.springbootdeveloper.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController // HTTP Response Body에 객체 데이터를 JSON 형식으로 반환하는 컨트롤러
@@ -25,5 +27,40 @@ public class BlogApiController {
         // 요청한 자원이 성공적으로 생성되었으며 저장된 블로그 글 정보를 응답 객체에 담아 전송
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedArticle);
+    }
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles(){
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
+    }
+    @GetMapping("/api/articles/{id}")
+    // URL 경로에서 값 추출
+    public ResponseEntity<ArticleResponse> findArticle(@PathVariable long id){ // URL에서 {id}가져옴
+        Article article = blogService.findById(id);
+
+        return ResponseEntity.ok()
+                .body(new ArticleResponse(article));
+    }
+
+    @DeleteMapping("/api/articles/{id}")
+    public ResponseEntity<Void> deleteArticle(@PathVariable long id){ // URL에서 {id}가져옴
+        blogService.delete(id);
+
+        return ResponseEntity.ok()
+                .build();
+    }
+
+    @PutMapping("/api/articles/{id}")
+    public ResponseEntity<Article> updateArticle(@PathVariable long id,
+                                                 @RequestBody UpdateArticleRequest request){
+        Article updatedArticle = blogService.update(id, request);
+
+        return ResponseEntity.ok()
+                .body(updatedArticle);
     }
 }
--- src/main/java/me/bowon/springbootdeveloper/service/BlogService.java
+++ src/main/java/me/bowon/springbootdeveloper/service/BlogService.java
@@ -1,10 +1,15 @@
 package me.bowon.springbootdeveloper.service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import me.bowon.springbootdeveloper.domain.Article;
 import me.bowon.springbootdeveloper.dto.AddArticleRequest;
+import me.bowon.springbootdeveloper.dto.UpdateArticleRequest;
 import me.bowon.springbootdeveloper.repository.BlogRepository;
 import org.springframework.stereotype.Service;
+
+import java.beans.Transient;
+import java.util.List;
 
 @RequiredArgsConstructor // final이 붙거나 @NotNull이 붙은 필드의 생성자 추가
 @Service // 빈으로 등록
@@ -17,5 +22,26 @@ public class BlogService {
         return blogRepository.save(request.toEntity());
     }
 
+    public Article findById(long id) {
+        return blogRepository.findById(id) // JPA findById()이용해서 ID를 받아 엔티티를 조회하고 IllegalArgumentException예외발생
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
 
+    }
+    public List<Article> findAll(){
+        return blogRepository.findAll();
+    }
+
+    public void delete(long id){
+        blogRepository.deleteById(id);
+    }
+
+    @Transactional // 트랜잭션 메서드 매칭한 메서드를 하나의 트랜잭션으로 묶음
+    public Article update(long id, UpdateArticleRequest request){
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("no found: " + id));
+
+        article.update(request.getTitle(), request.getContent());
+
+        return article;
+    }
 }
